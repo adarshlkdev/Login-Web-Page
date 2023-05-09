6704288113e95c3cce04415724a0fb882dc72e6a
adarshlkdev <erlkadarsh@gmail.com>
2023-05-10 01:25:40 +0530

    It is basic sign up page , created with the help of HTML , CSS & Javascript.

---
+++ Login or Sign up Page/script.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector(".wrapper"),
+          signupHeader = document.querySelector(".signup header"),
+          loginHeader = document.querySelector(".login header");
+        loginHeader.addEventListener("click", () => {
+          wrapper.classList.add("active");
+        });
+        signupHeader.addEventListener("click", () => {
+          wrapper.classList.remove("active");
+        });
